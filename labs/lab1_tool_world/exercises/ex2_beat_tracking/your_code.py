f629576a27ef564140a223756016cdf0facf5eb5
Michele Buccoli <m.buccoli@gmail.com>
2021-09-21 23:01:16 +0200

    add lab1 ex 2

---
+++ labs/lab1_tool_world/exercises/ex2_beat_tracking/your_code.py
@@ -0,0 +1,11 @@
+# any import? 
+
+def compute_beats(y, sr):
+    # your code here
+    return 0
+def add_samples(y, sample, beats):
+    y_out=y.copy()
+    # your code here ...
+
+    return y_out
+
