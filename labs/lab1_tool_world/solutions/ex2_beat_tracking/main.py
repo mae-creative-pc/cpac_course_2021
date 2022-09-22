8c4ad3e411ce291dfc61fb0218b23eca5a9ae979
Michele Buccoli <michele.buccoli@bdsound.com>
2022-09-22 23:50:47 +0200

    better plots

--- labs/lab1_tool_world/solutions/ex2_beat_tracking/main.py
+++ labs/lab1_tool_world/solutions/ex2_beat_tracking/main.py
@@ -7,7 +7,7 @@ from librosa import load, frames_to_samples
 import soundfile as sf
 import matplotlib.pyplot as plt
 from librosa.onset import onset_strength
-
+plt.style.use("seaborn-v0_8-poster")
 os.chdir(os.path.abspath(os.path.dirname(__file__)))
 # This is useful to change the current directory to the one where there is the file
 import your_code
@@ -23,13 +23,21 @@ filename_out=os.path.join(DATA_DIR, "tire_disco.wav") #
 
 SR=16000
 y, sr= load(filename_in, sr=SR)
+t=np.arange(y.size)/SR
+plt.figure(figsize=(8,3))
+plt.plot(t, y, label="y")
+plt.xlim([0, 5])
+plt.xlabel("Time [s]")
+plt.legend()
+plt.show()
+
+
 # %% 2) Find the beats
 bpm, beats=your_code.compute_beats(y, sr=SR)
 beats=frames_to_samples(beats)
 # %% See if beats are correct
-t=np.arange(y.size)/SR
 
-plt.figure()
+plt.figure(figsize=(8,3))
 
 plt.plot(t, y, label="y")
 plt.vlines(t[beats], -1,1, label="beats", color="red")
@@ -44,14 +52,15 @@ kick/=np.max(np.abs(kick))
 
 y_out = your_code.add_samples(y, kick, beats)
 # %% show the difference
-plt.figure()
+plt.figure(figsize=(8,7))
 plt.subplot(2,1,1)
 plt.plot(t, y)
 plt.xlim([0, 5])
 plt.xlabel("Time [s]")
 plt.title("$y(n)$")
 plt.subplot(2,1,2)
-plt.plot(t, y_out)
+plt.plot(t, y)
+plt.plot(t, 2.19*y_out-y)
 plt.title("$\\tilde{y}(n)$")
 plt.xlim([0, 5])
 plt.xlabel("Time [s]")
